fb0e525dc63518ace3b3fd94888226084609f8be
Adarsh <72968967+Adarshkds@users.noreply.github.com>
2023-10-25 17:17:23 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from lxml import html #We can also use BeautifulSoup for parsing the page source
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')  
+selenium_service = Service('chromedriver.exe')  
+driver = webdriver.Chrome(service=selenium_service, options=chrome_options)
+
+product_links = ["link1", "link2", ...]  # Replace with your actual product links
+count = 0
+for product_link in product_links:
+    try:
+        driver.get(product_link)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'zoomImg')))
+        page_source = driver.page_source
+        tree = html.fromstring(page_source)
+        zoom_img_element = tree.xpath('//img[contains(@class, "zoomImg")]')
+
+        if zoom_img_element:
+            image_src = zoom_img_element[0].get('src')
+            print("Product Image Source:", image_src)
+            count+=1
+            print(count)
+        else:
+            print("Image source not found on", product_link)
+
+    except TimeoutException:
+        print("Timed out while waiting for 'zoomImg' element on", product_link)
+
+driver.quit()
